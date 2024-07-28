a3fd960884eecfdaa3866e7d9c9e109f60f71e6d
patrick muthii <murimipatrick101@gmail.com>
2024-07-28 05:21:14 -0700

    new changes

--- task_6.sql
+++ task_6.sql
@@ -2,6 +2,6 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessin Malik', 'bmalik@sandtech.com', '123 Happiness Ave.')
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '123 Happiness Ave.')
        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '123 Happiness Ave.')
        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '123 Happiness Ave.')
