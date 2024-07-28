35f0b6b5b6ad39dffe383fda572fe6fcca3430f3
patrick muthii <murimipatrick101@gmail.com>
2024-07-28 04:38:38 -0700

    added new changes

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 
 SELECT 
     COLUMN_NAME, 
-    DATA_TYPE, 
+    COLUMN_TYPE,
     IS_NULLABLE, 
     COLUMN_DEFAULT, 
     COLUMN_KEY, 
@@ -9,5 +9,5 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = DATABASE() 
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'books';
